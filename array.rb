51e55c8747759c8c51ee6a64f8cd37e8e3813c2b
Alyssampratt <magoopratt@gmail.com>
2018-06-20 14:47:23 +0000

    automatically backed up by learn

--- array.rb
+++ array.rb
@@ -1,6 +1,9 @@
-student_one = "Caroline"
-student_two = "millie"
-student_three = "sam"
-student_four = "sonali"
+# student_one = "Caroline"
+# student_two = "millie"
+# student_three = "sam"
+# student_four = "sonali"
 
-puts "in this class, we have a student name #{student_four}"
+# puts "in this class, we have a student name #{student_four}"
+
+student_list = ["caroline", "millie", "sonali", "sam"]
+puts student_list
