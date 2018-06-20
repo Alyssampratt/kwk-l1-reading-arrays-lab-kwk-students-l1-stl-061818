a2dca84048975f473d8e46dae4d290ae0b47b847
Alyssampratt <magoopratt@gmail.com>
2018-06-20 15:40:17 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,19 +1,10 @@
-STUDENT_NAMES = [
+student_names = [
   "Adele",
   "Beyoncé",
   "Cardi B",
   "Lady Gaga",
   "Nicki Minaj",
-  "Rihanna"
-]
+  "Rihanna"]
 
 def first_student_by_index
-  puts STUDENT_NAMES[0]
-  puts STUDENT_NAMES[3]
-  puts STUDENT_NAMES[6]
-end
-
-
-def first_second_and_third_students
- puts STUDENT_NAMES[0,1,2]
-end
+  puts student_names[0]
