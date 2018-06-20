d6f66defa3054961885b5490254b5fb04b2821a8
Alyssampratt <magoopratt@gmail.com>
2018-06-20 15:29:46 +0000

    automatically backed up by learn

--- array.rb
+++ array.rb
@@ -1,26 +1,26 @@
-# student_one = "Caroline"
-# student_two = "millie"
-# student_three = "sam"
-# student_four = "sonali"
+# # student_one = "Caroline"
+# # student_two = "millie"
+# # student_three = "sam"
+# # student_four = "sonali"
 
-# puts "in this class, we have a student name #{student_four}"
+# # puts "in this class, we have a student name #{student_four}"
 
-# student_list = ["caroline", "millie", "sonali", "sam"]
-# # puts student_list
+# # student_list = ["caroline", "millie", "sonali", "sam"]
+# # # puts student_list
 
-# friends = ["ross", "rachel","chandler", "monica","phoebe"]
-# puts friends
+# # friends = ["ross", "rachel","chandler", "monica","phoebe"]
+# # puts friends
 
-disney = ["anna","belle","simba","olaf","ariel"]
-disney.push("pooh bear")
-disney.push("tigger")
-puts disney.size
+# disney = ["anna","belle","simba","olaf","ariel"]
+# disney.push("pooh bear")
+# disney.push("tigger")
+# puts disney.size
 
-age = [13,16,14]
-# puts age
+# age = [13,16,14]
+# # puts age
 
-books = ["fish out of water","Princess X","purple hibiscus"]
-# puts books[0]
+# books = ["fish out of water","Princess X","purple hibiscus"]
+# # puts books[0]
 
-books.push("geometry textbook")
-# puts books
+# books.push("geometry textbook")
+# # puts books
