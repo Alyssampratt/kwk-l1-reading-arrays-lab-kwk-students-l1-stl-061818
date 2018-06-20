e699da3c6337daa2b5537bd212e359a7b0223cab
Alyssampratt <magoopratt@gmail.com>
2018-06-20 15:44:33 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -7,5 +7,23 @@ student_names = [
   "Rihanna"]
 
 def first_student_by_index
-  puts student_names[0]
+  puts student_names.first 
 end
+first_student_by_index
+
+def last_student_by_index
+  puts student_names.last
+end 
+last_student_by_index
+
+def fourth_student_by_index
+  puts student_names[3]
+end
+fourth_student_by_index
+
+def first_second_and_third_students
+  puts student_names.first
+  puts student_names[1]
+  puts student_names[2]
+end 
+first_second_and_third_students
